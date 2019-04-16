68ec15a204a302fcbbdb92e0601f204944a435b7
Rjavey <1099779409@qq.com>
2019-04-16 23:56:21 +0800

    1

--- src/main/java/com/eureka/clienttest/ClientTestApplication.java
+++ src/main/java/com/eureka/clienttest/ClientTestApplication.java
@@ -3,9 +3,12 @@ package com.eureka.clienttest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +17,12 @@ public class ClientTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientTestApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
     @Value("${server.port}")
